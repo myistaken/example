80127ae9dc19264f1337d6cb0f4372b63b191f33
Mustafa YILMAZ <myiistaken@gmail.com>
2022-08-01 16:39:31 +0300

    first commit

---
+++ build/app/generated/source/buildConfig/debug/hamza/dali/flutter_osm_plugin_example/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package hamza.dali.flutter_osm_plugin_example;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "hamza.dali.flutter_osm_plugin_example";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
